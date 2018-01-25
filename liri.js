aa30195af86e9687a6a4eebeaa296b881f8d8082
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2018-01-25 13:55:57 -0600

    OMDB request complete. Random text method started. Psuedocode added.

--- liri.js
+++ liri.js
@@ -1,61 +1,141 @@
 // JavaScript Document
+
 //node require
 require("dotenv").config();
-
 var keys = require("./keys.js");
 var Twitter = require('twitter');
-var spotify = require('spotify');
+var Spotify = require('spotify');
 var request = require ('request');
 var dotenv = require('dotenv');
+var fs = require('fs');
+var cmd=require('node-cmd');
 
 //global variables
 var command = process.argv[2];
+var userVariable = process.argv[3];
+var randomData = null;
 
 //params
 var tParams = {screen_name: 'devtechconnect'};
 
 //constructor functions
-function Spotify(id, secret) {
-	this.id = id;
-	this.secret = secret
-};
+//function Spotify(id, secret) {
+//	this.id = id;
+//	this.secret = secret
+//};
 
-//api keys
-var spotify = new Spotify(keys.spotify.id, keys.spotify.secret);
+//api key objects
+//var spotify = new Spotify({
+//	id: keys.spotify.id, 
+//	secret: keys.spotify.secret,
+//});
 var client = new Twitter({
 	consumer_key: keys.twitter.consumer_key, 
 	consumer_secret: keys.twitter.consumer_secret, 
 	access_token_key: keys.twitter.access_token_key, 
-	access_token_secret: keys.twitter.access_token_secret
+	access_token_secret: keys.twitter.access_token_secret,
 });
 
 //console.log(spotify);
 
 //methods object
 var methods = {
+	//twitter method
 	checkTweets: function() {
 		client.get('statuses/user_timeline', { screen_name: 'devtechconnect', count: 20 }, function(error, tweets, response) {
+			//if no error
 			if (!error) {
-				//console.log(tweets);
+				//response loop
 				for (var i = 0; i < tweets.length; i++) {
+					//response variables
 					var createdAt = tweets[i].created_at
 					var created = createdAt.substr(0, 16);
+					var tweet = tweets[i].text
+					//console log response
 					console.log("============================================");
 					console.log("Date: " + created);
-					console.log("Tweet: " + tweets[i].text);
+					console.log("Tweet: " + tweet);
 				};
 			}
+			//else return error
 			else {
 				console.log(error);
 			}
 		});
 	},
-	songInfo: function() {},
-	movieInfo: function() {},
-	randomText: function() {},
-}
+	//spotify funtion
+	songInfo: function() {
+		spotify.search({ type: 'track', query: userVariable}, function(err, data) {
+			//error handling
+			if (err) {
+				console.log('Error occured: ' + err);
+				return;
+			}
+			//response
+			console.log(data);
+		})
+	},
+	//omdb method
+	movieInfo: function() {
+		//if no user input regarding movie title
+		if (userVariable == null) {
+			console.log("============================================");
+			console.log("If you haven't watched 'Mr. Nobody,' you should: <http://www.imdb.com/title/tt0485947/>");
+			console.log("It's on Netflix!");
+			userVariable = "Mr. Nobody";
+		};
+		//omdb request
+		request("http://www.omdbapi.com/?t=" + userVariable + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+			//if no error
+  			if (!error && response.statusCode === 200) {
+				//variables
+				var title = JSON.parse(body).Title;
+				var release = JSON.parse(body).Year;
+				var imdbRating = JSON.parse(body).imdbRating;
+				var ratings = JSON.parse(body).Ratings;
+				var country = JSON.parse(body).Country;
+				var language = JSON.parse(body).Language;
+				var plot = JSON.parse(body).Plot;
+				var actors = JSON.parse(body).Actors;
+				//console log responses
+				console.log("============================================");
+				console.log("Title: " + title);
+				console.log("Release Year: " + release);
+				console.log("IMDB Rating: " + imdbRating);
+				//Rotten Tomatoes rating loop
+				for (var j = 0; j < ratings.length; j++) {
+					if (ratings[j].Source == 'Rotten Tomatoes') {
+						var rottenIndex = j;
+						console.log(console.log("Rotten Tomatoes Rating: " + ratings[rottenIndex].Value));
+						break;
+					};
+				};
+				console.log("Produced In: " + country);
+				console.log("Language: " + language);
+				console.log("Plot: " + plot);
+				console.log("Actors: " + actors);
+				console.log("============================================");
+  			}
+		});
+	},
+	//read random text method
+	randomText: function() {
+		fs.readFile("random.txt", "utf8", function(err, data) {
+			//error handling
+			if (err) {
+				console.log(err);
+			};
+			//global variable update
+			randomData = data
+			//console log response
+			console.log(randomData);
+			//run node liri.js based on text in random.txt
+			cmd.run('node liri.js "' + randomData + "'");
+		});
+	},
+};
 
-
+//switch statement to update command global variable and call function
 switch (command) {
 	case 'my-tweets':
 		methods.checkTweets();
@@ -70,4 +150,7 @@ switch (command) {
 		methods.randomText();
 		break;
 };
+
+
+
 
